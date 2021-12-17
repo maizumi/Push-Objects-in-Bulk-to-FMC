1f0353f0306c92504df5c8f56c84395f6d255534
maizumi <maizumi@cisco.com>
2021-12-17 11:52:32 +0900

    Update and rename bulk_upload_webapp to bulk_upload_webapp.py

--- bulk_upload_webapp.py
+++ bulk_upload_webapp.py
@@ -13,8 +13,8 @@ from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = './uploads' #where upload file send to
-ALLOWED_EXTENSIONS = set(['csv']) #restrict file type only csv
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = set(['csv'])
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/', methods=['GET'])
@@ -32,12 +32,23 @@ def send():
         if csv_file and allowed_file(csv_file.filename):
             filename = secure_filename(csv_file.filename)
             csv_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            #csv_url = '/uploads/' + filename
             csv_url = app.config['UPLOAD_FOLDER'] + '/' + filename
+            abspath  = os.path.abspath(filename)
             #print(csv_url)
+            #print(abspath)
+            
+            fmc_ip = request.form.get('fmc_ip')
+            fmc_username = request.form.get('fmc_username')
+            fmc_password = request.form.get('fmc_password')
+         
+            #address = "10.71.132.226"
+            #username = "admin"
+            #password = "C1sco12345!"
+            address = fmc_ip
+            username = fmc_username
+            password = fmc_password
                         
-            address = "10.71.132.226"
-            username = "admin"
-            password = "C1sco12345!"
             api_uri = "/api/fmc_platform/v1/auth/generatetoken"
             url = "https://" + address + api_uri
             
@@ -51,6 +62,8 @@ def send():
             csvFilePath = csv_url
             
             host = []
+            network = []
+            nwrange = []
             
             with open(csvFilePath, encoding='utf-8-sig') as csvf:
             	csvReader = csv.DictReader(csvf)
@@ -59,15 +72,37 @@ def send():
             		if rows['type'] == "Host":
             			host.append(rows)
             			host_payload = json.dumps(host)
-            	
+            		
+            		if rows['type'] == "Network":
+            			network.append(rows)
+            			network_payload = json.dumps(network)
+
+            		if rows['type'] == "Range":
+            			nwrange.append(rows)
+            			nwrange_payload = json.dumps(nwrange)
+            	         			            		            	
             	host_api_uri = "/api/fmc_config/v1/domain/" + DOMAIN_UUID + "/object/hosts?bulk=true"
             	host_url = "https://" + address + host_api_uri
-            	headers = {'Content-Type': 'application/json', 'x-auth-access-token': accesstoken}
-            	print(headers)
             	
+            	network_api_uri =  "/api/fmc_config/v1/domain/" + DOMAIN_UUID + "/object/networks?bulk=true"
+            	network_url = "https://" + address + network_api_uri
+            	
+            	nwrange_api_uri =  "/api/fmc_config/v1/domain/" + DOMAIN_UUID + "/object/ranges?bulk=true"
+            	nwrange_url = "https://" + address + nwrange_api_uri
+            	            	
+            	headers = {'Content-Type': 'application/json', 'x-auth-access-token': accesstoken}            	
+            	
+            	#print(host_payload, network_payload, nwrange_payload)            	
             	
             	if host != []:
             		response = requests.request("POST", host_url, headers=headers, data=host_payload, verify=False)
+            		
+            		if network != []:
+            		    response = requests.request("POST", network_url, headers=headers, data=network_payload, verify=False)
+            		    
+            		    if nwrange != []:
+            		        response = requests.request("POST", nwrange_url, headers=headers, data=nwrange_payload, verify=False)
+            		            		
             	else:
             		print("Please Validate that the CSV file provided is correct or at correct location")
             	
